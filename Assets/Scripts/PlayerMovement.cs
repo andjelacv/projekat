932ef7c657f4185c9f19e45cccb59de547ce97b3
andjelacv <andjela.cvijanovic16@gmail.com>
2024-01-26 03:50:45 +0100

    sakupljanje voca

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -7,8 +7,11 @@ public class PlayerMovement : MonoBehaviour
 
     private float dirx;
     private Rigidbody2D rb;
+    private BoxCollider2D collider;
     private Animator anim;
     private SpriteRenderer sprite;
+
+    [SerializeField] private LayerMask jumpableGround;
 
     [SerializeField] private float moveSpeed = 7f;
     [SerializeField] private float jumpFloat = 14f;
@@ -22,6 +25,7 @@ public class PlayerMovement : MonoBehaviour
         rb=GetComponent<Rigidbody2D>();
         anim=GetComponent<Animator>();
         sprite= GetComponent<SpriteRenderer>();
+        collider= GetComponent<BoxCollider2D>();
     }
 
     // Update is called once per frame
@@ -31,7 +35,7 @@ public class PlayerMovement : MonoBehaviour
         dirx = Input.GetAxisRaw("Horizontal");
         rb.velocity= new Vector2(dirx*moveSpeed,rb.velocity.y);
 
-        if (Input.GetButtonDown("Jump"))
+        if (Input.GetButtonDown("Jump") && isGrounded())
         {
             rb.velocity = new Vector2(rb.velocity.x, jumpFloat);
         }
@@ -75,4 +79,11 @@ public class PlayerMovement : MonoBehaviour
 
 
     }
+
+    private bool isGrounded()
+    {
+        return Physics2D.BoxCast(collider.bounds.center, collider.bounds.size, 0f, Vector2.down, .1f,jumpableGround);
+    }
+
+
 }
